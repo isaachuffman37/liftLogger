83417eeaadc3a5d62faa536b6de06883c6559e3f
Branislav Bogosavac <97567320+branislav999@users.noreply.github.com>
2024-12-07 16:38:26 -0700

    donelogin

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const user = require('./users');
 const layout = require('./layout')
+const login = require('./login');
 
 router.use(require("./static"))
 router.use('/', layout)
+router.use('/', login)
 router.use('/', require('./swagger'));
 router.use('/', require('./workoutHistory'))
 router.use('/users', user);
